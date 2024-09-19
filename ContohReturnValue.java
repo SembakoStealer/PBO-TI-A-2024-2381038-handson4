ed08cb50140827f26481e3f61a35971e6d1f16a5
Joyada Okomoto <2381038@unai.edu>
2024-09-19 10:29:26 +0700

    Handson 4

---
+++ ContohReturnValue.java
@@ -0,0 +1,13 @@
+public class ContohReturnValue {
+    public static void sebutkanNama (){
+        System.out.println("Nama saya Joyada Okomoto");
+    }
+    public static int kembalikanAngka (){
+        int hasilPerkalian = 20 * 3;
+        return  hasilPerkalian;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
